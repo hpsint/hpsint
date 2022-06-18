437169713c4f32c47b303da6816968c4d4134e84
Peter Munch <peterrmuench@gmail.com>
2022-06-18 11:08:50 +0200

    Switch to unique ptr for timer

--- include/pf-applications/base/timer.h
+++ include/pf-applications/base/timer.h
@@ -4,18 +4,19 @@ class MyScope
 {
 public:
   MyScope(dealii::TimerOutput &timer_, const std::string &section_name)
-#ifdef WITH_TIMING
-    : scope(timer_, section_name)
-#endif
   {
+#ifdef WITH_TIMING
+    scope = std::make_unique<dealii::TimerOutput::Scope>(timer_, section_name);
+#else
     (void)timer_;
     (void)section_name;
+#endif
   }
 
   ~MyScope() = default;
 
 private:
 #ifdef WITH_TIMING
-  dealii::TimerOutput::Scope scope;
+  std::unique_ptr<dealii::TimerOutput::Scope> scope;
 #endif
 };
