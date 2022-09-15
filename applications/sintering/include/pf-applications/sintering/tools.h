988591475dc8ecc8960704035108bd8d85df4386
Vladimir Ivannikov <vovannikov@yandex.ru>
2022-09-15 22:08:30 +0200

    Create array helper

--- applications/sintering/include/pf-applications/sintering/tools.h
+++ applications/sintering/include/pf-applications/sintering/tools.h
@@ -324,4 +324,22 @@ namespace Sintering
     return cross_product_3d(x, y);
   }
 
+  namespace internal
+  {
+    template <typename T, std::size_t... Is>
+    constexpr std::array<T, sizeof...(Is)>
+    create_array(T value, std::index_sequence<Is...>)
+    {
+      // cast Is to void to remove the warning: unused value
+      return {{(static_cast<void>(Is), value)...}};
+    }
+  } // namespace internal
+
+  template <std::size_t N, typename T>
+  constexpr std::array<T, N>
+  create_array(const T &value)
+  {
+    return internal::create_array(value, std::make_index_sequence<N>());
+  }
+
 } // namespace Sintering
