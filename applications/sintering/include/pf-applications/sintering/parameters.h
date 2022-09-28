a9a00a8a41c0f4ec0359a391bc2bd3af0b95b849
Vladimir Ivannikov <vovannikov@yandex.ru>
2022-09-28 13:17:37 +0200

    Fix code format

--- applications/sintering/include/pf-applications/sintering/parameters.h
+++ applications/sintering/include/pf-applications/sintering/parameters.h
@@ -610,9 +610,7 @@ namespace Sintering
       prm.add_parameter("Shrinkage",
                         output_data.shrinkage,
                         "Determine shrinkage.");
-      prm.add_parameter("Table",
-                        output_data.table,
-                        "Output table.");
+      prm.add_parameter("Table", output_data.table, "Output table.");
       prm.add_parameter("Debug",
                         output_data.debug,
                         "Whether debug output is enabled.");
