89de4d1f9eedb22c82d6b5bdc417fa26a47ece3b
Vladimir Ivannikov <vovannikov@yandex.ru>
2022-09-30 11:44:20 +0200

    Clean up generic operator

--- applications/sintering/include/pf-applications/sintering/operator_sintering_generic.h
+++ applications/sintering/include/pf-applications/sintering/operator_sintering_generic.h
@@ -1,11 +1,6 @@
 #pragma once
 
-#include <pf-applications/sintering/advection.h>
-#include <pf-applications/sintering/operator_base.h>
 #include <pf-applications/sintering/operator_sintering_base.h>
-#include <pf-applications/sintering/sintering_data.h>
-
-#include <pf-applications/time_integration/solution_history.h>
 
 namespace Sintering
 {
