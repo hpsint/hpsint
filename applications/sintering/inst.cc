c4de4879885f0bbc00945453c7dcb05db276ffb6
Vladimir Ivannikov <vovannikov@yandex.ru>
2022-10-26 08:11:24 +0200

    Code format

--- applications/sintering/inst.cc
+++ applications/sintering/inst.cc
@@ -1,10 +1,11 @@
-#include <memory>
-#include <vector>
+#include <pf-applications/lac/dynamic_block_vector.h>
 
 #include <deal.II/trilinos/nox.h>
 #include <deal.II/trilinos/nox.templates.h>
-#include <pf-applications/lac/dynamic_block_vector.h>
+
+#include <memory>
 
 using namespace dealii;
 
-template class TrilinosWrappers::NOXSolver<LinearAlgebra::distributed::DynamicBlockVector<double>>;
+template class TrilinosWrappers::NOXSolver<
+  LinearAlgebra::distributed::DynamicBlockVector<double>>;
