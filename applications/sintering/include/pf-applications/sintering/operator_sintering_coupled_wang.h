b59867bcb7f32d6f0ab9a7f149f633af3582ba73
Vladimir Ivannikov <vovannikov@yandex.ru>
2022-11-21 18:52:37 +0100

    Change call

--- applications/sintering/include/pf-applications/sintering/operator_sintering_coupled_wang.h
+++ applications/sintering/include/pf-applications/sintering/operator_sintering_coupled_wang.h
@@ -81,7 +81,7 @@ namespace Sintering
 
       // Apply manual constraints
       for (unsigned int d = 0; d < dim; ++d)
-        for (const unsigned int index : displ_constraints_indices[d])
+        for (const unsigned int index : this->get_zero_constraints_indices()[d])
           dst.block(this->data.n_components() + d).local_element(index) = 0.0;
     }
 
