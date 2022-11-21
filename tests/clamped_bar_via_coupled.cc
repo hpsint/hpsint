74ad279b761597a36b4f920564f22f7b5c26afed
Vladimir Ivannikov <vovannikov@yandex.ru>
2022-11-21 19:19:42 +0100

    Fix lambdas

--- tests/clamped_bar_via_coupled.cc
+++ tests/clamped_bar_via_coupled.cc
@@ -413,14 +413,15 @@ public:
       std::cout << std::endl;
     };
 
-    non_linear_solver->setup_jacobian =
-      [&](const auto &current_u, const bool do_update_preconditioner) {
-        (void)current_u;
-        nonlinear_operator.do_update();
+    non_linear_solver->setup_jacobian = [&](const auto &current_u) {
+      (void)current_u;
+      nonlinear_operator.do_update();
+    };
 
-        if (do_update_preconditioner)
-          preconditioner->do_update();
-      };
+    non_linear_solver->setup_preconditioner = [&](const auto &current_u) {
+      (void)current_u;
+      preconditioner->do_update();
+    };
 
     non_linear_solver->solve_with_jacobian = [&](const auto &src, auto &dst) {
       const unsigned int n_iterations = linear_solver->solve(dst, src);
