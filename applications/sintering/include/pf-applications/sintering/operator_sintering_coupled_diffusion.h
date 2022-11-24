7759ec6408d7588bce1bd0b725645436e6b15d92
Vladimir Ivannikov <vovannikov@yandex.ru>
2022-11-24 17:07:54 +0100

    Change dSdE() call

--- applications/sintering/include/pf-applications/sintering/operator_sintering_coupled_diffusion.h
+++ applications/sintering/include/pf-applications/sintering/operator_sintering_coupled_diffusion.h
@@ -235,9 +235,7 @@ namespace Sintering
                             n_additional_components() + d];
 
           const auto E = apply_l(H);
-
-          const auto C = c * this->material.get_dSdE();
-
+          const auto C = this->dSdE(E, c);
           const auto S = Structural::apply_l_transposed<dim>(C * E);
 
           for (unsigned int d = 0; d < dim; d++)
@@ -398,11 +396,7 @@ namespace Sintering
               H += eps_inelastic;
 
               const auto E = apply_l(H);
-
-              // update material
-              this->material.reinit(E);
-              const auto C = c * this->material.get_dSdE();
-
+              const auto C = this->dSdE(E, c);
               const auto S = Structural::apply_l_transposed<dim>(C * E);
 
               for (unsigned int d = 0; d < dim; d++)
