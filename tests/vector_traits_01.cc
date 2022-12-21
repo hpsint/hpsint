8e14ab98b196893043b14d947dec78d64c46a2b5
Peter Munch <peterrmuench@gmail.com>
2022-12-21 16:57:55 +0100

    Continue

--- tests/vector_traits_01.cc
+++ tests/vector_traits_01.cc
@@ -5,30 +5,34 @@
 
 using namespace dealii;
 
+
 template <typename VectorType>
+constexpr bool is_dealii_compatible_vector =
+  std::is_same<
+    VectorType,
+    LinearAlgebra::distributed::Vector<typename VectorType::value_type,
+                                       MemorySpace::Host>>::value ||
+  std::is_same<VectorType,
+               LinearAlgebra::distributed::BlockVector<
+                 typename VectorType::value_type>>::value ||
+  std::is_same<VectorType,
+               LinearAlgebra::distributed::DynamicBlockVector<
+                 typename VectorType::value_type>>::value;
+
+template <typename VectorType,
+          std::enable_if_t<!is_dealii_compatible_vector<VectorType>, VectorType>
+            * = nullptr>
 void
 test(const VectorType &)
 {
   std::cout << "Fail!" << std::endl;
 }
 
-template <typename Number>
+template <typename VectorType,
+          std::enable_if_t<is_dealii_compatible_vector<VectorType>, VectorType>
+            * = nullptr>
 void
-test(const LinearAlgebra::distributed::Vector<Number> &)
-{
-  std::cout << "Success!" << std::endl;
-}
-
-template <typename Number>
-void
-test(const LinearAlgebra::distributed::BlockVector<Number> &)
-{
-  std::cout << "Success!" << std::endl;
-}
-
-template <typename Number>
-void
-test(const LinearAlgebra::distributed::DynamicBlockVector<Number> &)
+test(const VectorType &)
 {
   std::cout << "Success!" << std::endl;
 }
