9e42605401310f282eb97a1b14c071663197a238
Peter Munch <peterrmuench@gmail.com>
2022-12-23 19:53:59 +0100

    Add timers

--- applications/sintering/sintering_throughput.cc
+++ applications/sintering/sintering_throughput.cc
@@ -39,6 +39,10 @@ static_assert(false, "No grains number has been given!");
 #include <deal.II/base/mpi.h>
 #include <deal.II/base/revision.h>
 
+#ifdef LIKWID_PERFMON
+#  include <likwid.h>
+#endif
+
 #include <pf-applications/base/revision.h>
 
 #include <pf-applications/sintering/advection.h>
@@ -49,43 +53,176 @@ static_assert(false, "No grains number has been given!");
 using namespace dealii;
 using namespace Sintering;
 
+static unsigned int likwid_counter;
+
 int
 main(int argc, char **argv)
 {
   Utilities::MPI::MPI_InitFinalize mpi_init(argc, argv, 1);
 
-  const unsigned int dim    = SINTERING_DIM;
-  using Number              = double;
-  using VectorizedArrayType = VectorizedArray<Number>;
+  const unsigned int dim                  = SINTERING_DIM;
+  const unsigned int fe_degree            = 1;
+  const unsigned int n_global_refinements = 3;
+  const unsigned int n_repetitions        = 100;
+  using Number                            = double;
+  using VectorizedArrayType               = VectorizedArray<Number>;
   using VectorType = LinearAlgebra::distributed::DynamicBlockVector<Number>;
 
   // some arbitrary constants
-  const double A                      = 16;
-  const double B                      = 1;
-  const double kappa_c                = 1;
-  const double kappa_p                = 0.5;
-  const double Mvol                   = 1e-2;
-  const double Mvap                   = 1e-10;
-  const double Msurf                  = 4;
-  const double Mgb                    = 0.4;
-  const double L                      = 1;
-  const double time_integration_order = 1;
+  const double        A                      = 16;
+  const double        B                      = 1;
+  const double        kappa_c                = 1;
+  const double        kappa_p                = 0.5;
+  const double        Mvol                   = 1e-2;
+  const double        Mvap                   = 1e-10;
+  const double        Msurf                  = 4;
+  const double        Mgb                    = 0.4;
+  const double        L                      = 1;
+  const double        time_integration_order = 1;
+  const double        t                      = 0.0;
+  const double        dt                     = 0.1;
+  std::vector<double> dts(time_integration_order, 0.0);
+  dts[0] = dt;
+
+  const std::string fe_type =
+#ifdef USE_FE_Q_iso_Q1
+    "FE_Q_iso_Q1";
+#else
+    "FE_Q";
+#endif
+
+  std::unique_ptr<FiniteElement<dim>> fe;
+  std::unique_ptr<Quadrature<dim>>    quadrature;
+  MappingQ1<dim>                      mapping;
+
+  if (fe_type == "FE_Q")
+    {
+      fe = std::make_unique<FE_Q<dim>>(fe_degree);
+
+      quadrature = std::make_unique<QGauss<dim>>(fe_degree + 1);
+    }
+  else if (fe_type == "FE_Q_iso_Q1")
+    {
+      AssertThrow(fe_degree == 1, ExcInternalError());
+
+      const unsigned int n_subdivisions = 2;
+
+      fe = std::make_unique<FE_Q_iso_Q1<dim>>(n_subdivisions);
+      quadrature =
+        std::make_unique<QIterated<dim>>(QGauss<1>(2), n_subdivisions);
+    }
+  else
+    {
+      AssertThrow(false, ExcNotImplemented());
+    }
+
+  parallel::distributed::Triangulation<dim> tria(MPI_COMM_WORLD);
+  GridGenerator::hyper_cube(tria);
+  tria.refine_global(n_global_refinements);
+
+  DoFHandler<dim> dof_handler(tria);
+  dof_handler.distribute_dofs(*fe);
 
   AffineConstraints<Number> constraints;
 
+  typename MatrixFree<dim, Number, VectorizedArrayType>::AdditionalData
+    additional_data;
+  additional_data.overlap_communication_computation = false;
+
   MatrixFree<dim, Number, VectorizedArrayType> matrix_free;
+  matrix_free.reinit(
+    mapping, dof_handler, constraints, *quadrature, additional_data);
 
-  const std::shared_ptr<MobilityProvider> mobility_provider =
-    std::make_shared<ProviderAbstract>(Mvol, Mvap, Msurf, Mgb, L);
+  // helper function
+  const auto run = [&](const auto fu) {
+    // warm up
+    for (unsigned int i = 0; i < 10; ++i)
+      fu();
 
-  TimeIntegration::SolutionHistory<VectorType> solution_history(
-    time_integration_order + 1);
+#ifdef LIKWID_PERFMON
+    const auto add_padding = [](const int value) -> std::string {
+      if (value < 10)
+        return "000" + std::to_string(value);
+      if (value < 100)
+        return "00" + std::to_string(value);
+      if (value < 1000)
+        return "0" + std::to_string(value);
+      if (value < 10000)
+        return "" + std::to_string(value);
 
-  SinteringOperatorData<dim, VectorizedArrayType> data(
-    A, B, kappa_c, kappa_p, mobility_provider, time_integration_order);
+      AssertThrow(false, ExcInternalError());
 
-  AdvectionMechanism<dim, Number, VectorizedArrayType> advection;
+      return "";
+    };
 
-  SinteringOperatorGeneric<dim, Number, VectorizedArrayType> op_sintering(
-    matrix_free, constraints, data, solution_history, advection, false);
+    const std::string likwid_label =
+      "likwid_" + add_padding(likwid_counter); // TODO
+    likwid_counter++;
+#endif
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+#ifdef LIKWID_PERFMON
+    LIKWID_MARKER_START(likwid_label.c_str());
+#endif
+
+    const auto timer = std::chrono::system_clock::now();
+
+    for (unsigned int i = 0; i < n_repetitions; ++i)
+      fu();
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+#ifdef LIKWID_PERFMON
+    LIKWID_MARKER_STOP(likwid_label.c_str());
+#endif
+
+    const double time = std::chrono::duration_cast<std::chrono::nanoseconds>(
+                          std::chrono::system_clock::now() - timer)
+                          .count() /
+                        1e9;
+
+    return time;
+  };
+
+  for (unsigned int n_grains = 2; n_grains < MAX_SINTERING_GRAINS; ++n_grains)
+    {
+      const std::shared_ptr<MobilityProvider> mobility_provider =
+        std::make_shared<ProviderAbstract>(Mvol, Mvap, Msurf, Mgb, L);
+
+      TimeIntegration::SolutionHistory<VectorType> solution_history(
+        time_integration_order + 1);
+
+      SinteringOperatorData<dim, VectorizedArrayType> sintering_data(
+        A, B, kappa_c, kappa_p, mobility_provider, time_integration_order);
+
+      sintering_data.set_n_components(n_grains + 2);
+      sintering_data.time_data.set_all_dt(dts);
+      sintering_data.set_time(t);
+
+      AdvectionMechanism<dim, Number, VectorizedArrayType> advection;
+
+      SinteringOperatorGeneric<dim, Number, VectorizedArrayType>
+        nonlinear_operator(matrix_free,
+                           constraints,
+                           sintering_data,
+                           solution_history,
+                           advection,
+                           false);
+
+      VectorType src, dst;
+      nonlinear_operator.initialize_dof_vector(src);
+      nonlinear_operator.initialize_dof_vector(dst);
+
+      src = 1.0;
+
+      sintering_data.fill_quadrature_point_values(matrix_free,
+                                                  src,
+                                                  false,
+                                                  false);
+
+      const auto time = run([&]() { nonlinear_operator.vmult(dst, src); });
+
+      std::cout << time << std::endl;
+    }
 }
