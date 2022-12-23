8863a090985cb3a922f5c0686515f853bb9543a7
Peter Munch <peterrmuench@gmail.com>
2022-12-23 14:46:55 +0100

    Add empty file

---
+++ applications/sintering/sintering_throughput.cc
@@ -0,0 +1,50 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2020 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE.md at
+// the top level directory of deal.II.
+//
+// ---------------------------------------------------------------------
+
+// Sintering of N particles loaded from a CSV file
+
+#ifndef SINTERING_DIM
+static_assert(false, "No dimension has been given!");
+#endif
+
+#ifndef MAX_SINTERING_GRAINS
+static_assert(false, "No grains number has been given!");
+#endif
+
+//#define USE_FE_Q_iso_Q1
+
+#ifdef USE_FE_Q_iso_Q1
+#  define FE_DEGREE 2
+#  define N_Q_POINTS_1D FE_DEGREE * 2
+#else
+#  define FE_DEGREE 1
+#  define N_Q_POINTS_1D FE_DEGREE + 1
+#endif
+
+#define WITH_TIMING
+//#define WITH_TIMING_OUTPUT
+
+#include <deal.II/base/mpi.h>
+#include <deal.II/base/revision.h>
+
+#include <pf-applications/base/revision.h>
+
+using namespace dealii;
+
+int
+main(int argc, char **argv)
+{
+  Utilities::MPI::MPI_InitFinalize mpi_init(argc, argv, 1);
+}
