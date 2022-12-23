98cd56a5d312229a48f385c942fa2a6fc4fe93e8
Vladimir Ivannikov <vovannikov@yandex.ru>
2022-12-23 13:43:06 +0100

    Add params

--- applications/sintering/include/pf-applications/sintering/parameters.h
+++ applications/sintering/include/pf-applications/sintering/parameters.h
@@ -160,6 +160,7 @@ namespace Sintering
     unsigned int          n_coarsening_steps     = 0;
     bool                  porosity               = false;
     bool                  shrinkage              = false;
+    bool                  quality                = false;
     bool                  table                  = false;
     bool                  debug                  = false;
     bool                  higher_order_cells     = false;
@@ -627,6 +628,7 @@ namespace Sintering
       prm.add_parameter("Shrinkage",
                         output_data.shrinkage,
                         "Determine shrinkage.");
+      prm.add_parameter("Quality", output_data.quality, "Output mesh quality.");
       prm.add_parameter("Table", output_data.table, "Output table.");
       prm.add_parameter("Debug",
                         output_data.debug,
