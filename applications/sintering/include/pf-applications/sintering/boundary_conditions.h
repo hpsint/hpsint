83d84b00db65436f3e51c786ed58905eb0da92ae
Vladimir Ivannikov <vovannikov@yandex.ru>
2022-12-27 18:05:15 +0100

    Remove omitted code

--- applications/sintering/include/pf-applications/sintering/boundary_conditions.h
+++ applications/sintering/include/pf-applications/sintering/boundary_conditions.h
@@ -348,26 +348,20 @@ namespace Sintering
     for (const auto &cell :
          matrix_free.get_dof_handler().active_cell_iterators())
       if (cell->is_locally_owned())
-        {
-          const auto cell_index = cell->global_active_cell_index();
+        for (const auto &face : cell->face_iterators())
+          if (face->at_boundary())
+            for (unsigned int d = 0; d < dim; ++d)
+              // Default colorization is implied
+              if (face->boundary_id() == 2 * d)
+                {
+                  face->get_dof_indices(local_face_dof_indices);
 
-          for (const auto &face : cell->face_iterators())
-
-            if (face->at_boundary())
-              for (unsigned int d = 0; d < dim; ++d)
-                // Default colorization is implied
-                if (face->boundary_id() == 2 * d)
-                  {
-                    face->get_dof_indices(local_face_dof_indices);
-
-                    for (const auto i : local_face_dof_indices)
-                      {
-                        const auto local_index =
-                          partitioner->global_to_local(i);
-                        displ_constraints_indices[d].push_back(local_index);
-                      }
-                  }
-        }
+                  for (const auto i : local_face_dof_indices)
+                    {
+                      const auto local_index = partitioner->global_to_local(i);
+                      displ_constraints_indices[d].push_back(local_index);
+                    }
+                }
   }
 
 
