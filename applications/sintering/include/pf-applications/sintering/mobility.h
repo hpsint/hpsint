98be22c9d1a244fc6b4bcab6ee813c3e94308b60
Peter Munch <peterrmuench@gmail.com>
2023-01-07 10:57:14 +0100

    Precompute factor

--- applications/sintering/include/pf-applications/sintering/mobility.h
+++ applications/sintering/include/pf-applications/sintering/mobility.h
@@ -723,15 +723,14 @@ namespace Sintering
             const auto eta_grad_diff = etas_grad[i] - etas_grad[j];
             const auto neta          = unit_vector(eta_grad_diff);
 
-            VectorizedArrayType neta_mu_grad = neta[0] * mu_grad[0];
-
+            auto neta_mu_grad = neta[0] * mu_grad[0];
             for (unsigned int d = 1; d < dim; ++d)
               neta_mu_grad += neta[d] * mu_grad[d];
 
+            const auto factor = etas[j] * value[i] + etas[i] * value[j];
+
             for (unsigned int d = 0; d < dim; ++d)
-              out[d] +=
-                (mu_grad[d] - neta[d] * neta_mu_grad) * etas[j] * value[i] +
-                (mu_grad[d] - neta[d] * neta_mu_grad) * etas[i] * value[j];
+              out[d] += (mu_grad[d] - neta[d] * neta_mu_grad) * factor;
           }
 
       return out * 2. * Mgb;
