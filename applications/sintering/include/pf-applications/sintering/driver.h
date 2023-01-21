b8dac5de675aeb644a15d49952e33ddeead919b0
Peter Munch <peterrmuench@gmail.com>
2023-01-21 11:56:01 +0100

    Make code compile again

--- applications/sintering/include/pf-applications/sintering/driver.h
+++ applications/sintering/include/pf-applications/sintering/driver.h
@@ -927,16 +927,32 @@ namespace Sintering
           params.preconditioners_data.block_preconditioner_2_data);
       else if (params.preconditioners_data.outer_preconditioner ==
                "BlockPreconditioner2")
-        preconditioner = std::make_unique<
-          BlockPreconditioner2<dim, Number, VectorizedArrayType>>(
-          sintering_data,
-          matrix_free,
-          constraints,
-          params.preconditioners_data.block_preconditioner_2_data,
-          advection_mechanism,
-          nonlinear_operator.get_zero_constraints_indices(),
-          params.material_data.mechanics_data.E,
-          params.material_data.mechanics_data.nu);
+        {
+          // TODO: find a safer solution
+          std::array<std::vector<unsigned int>, dim> displ_constraints_indices;
+          auto displ_constraints_indices_ptr = &displ_constraints_indices;
+
+
+          if constexpr (std::is_base_of_v<
+                          SinteringOperatorCoupledBase<dim,
+                                                       Number,
+                                                       VectorizedArrayType,
+                                                       NonLinearOperator>,
+                          NonLinearOperator>)
+            displ_constraints_indices_ptr =
+              &nonlinear_operator.get_zero_constraints_indices();
+
+          preconditioner = std::make_unique<
+            BlockPreconditioner2<dim, Number, VectorizedArrayType>>(
+            sintering_data,
+            matrix_free,
+            constraints,
+            params.preconditioners_data.block_preconditioner_2_data,
+            advection_mechanism,
+            *displ_constraints_indices_ptr,
+            params.material_data.mechanics_data.E,
+            params.material_data.mechanics_data.nu);
+        }
       else
         preconditioner = Preconditioners::create(
           nonlinear_operator, params.preconditioners_data.outer_preconditioner);
