51fe9ad4a90978acf381abbffd82cc5e9aac03ce
Vladimir Ivannikov <vovannikov@yandex.ru>
2023-01-27 16:58:05 +0100

    Clean up

--- applications/sintering/include/pf-applications/sintering/postprocessors.h
+++ applications/sintering/include/pf-applications/sintering/postprocessors.h
@@ -461,67 +461,6 @@ namespace Sintering
         vector.zero_out_ghost_values();
     }
 
-
-    template <int dim, typename VectorType>
-    typename VectorType::value_type
-    compute_surface_area(const Mapping<dim> &   mapping,
-                         const DoFHandler<dim> &background_dof_handler,
-                         const VectorType &     vector,
-                         const double           iso_level,
-                         const unsigned int     n_coarsening_steps = 0,
-                         const unsigned int     n_subdivisions     = 1,
-                         const double           tolerance          = 1e-10)
-    {
-      const auto only_concentration = vector.create_view(0, 1);
-
-      const bool has_ghost_elements = only_concentration->has_ghost_elements();
-
-      if (has_ghost_elements == false)
-        only_concentration->update_ghost_values();
-
-      auto vector_to_be_used                 = only_concentration.get();
-      auto background_dof_handler_to_be_used = &background_dof_handler;
-
-      parallel::distributed::Triangulation<dim> tria_copy(
-        background_dof_handler.get_communicator());
-      DoFHandler<dim> dof_handler_copy;
-      VectorType      solution_dealii;
-
-      if (n_coarsening_steps != 0)
-        {
-          internal::coarsen_triangulation(tria_copy,
-                                          background_dof_handler,
-                                          dof_handler_copy,
-                                          *only_concentration,
-                                          solution_dealii,
-                                          n_coarsening_steps);
-
-          vector_to_be_used                 = &solution_dealii;
-          background_dof_handler_to_be_used = &dof_handler_copy;
-        }
-
-      // step 1) create surface mesh
-      std::vector<Point<dim>>        vertices;
-      std::vector<CellData<dim - 1>> cells;
-      SubCellData                    subcelldata;
-
-      const GridTools::MarchingCubeAlgorithm<dim,
-                                             typename VectorType::BlockType>
-        mc(mapping,
-           background_dof_handler_to_be_used->get_fe(),
-           n_subdivisions,
-           tolerance);
-
-      for (const auto &cell :
-           background_dof_handler_to_be_used->active_cell_iterators())
-        if (cell->is_locally_owned())
-          {
-            const unsigned int old_size = cells.size();
-
-            mc.process_cell(
-              cell, vector_to_be_used->block(0), iso_level, vertices, cells);
-          }
-
     template <int dim, typename VectorType>
     typename VectorType::value_type
     compute_surface_area(const Mapping<dim> &   mapping,
