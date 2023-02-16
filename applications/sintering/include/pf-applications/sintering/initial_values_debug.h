da3c3a74366929c1e0afbe38d435af70bf316117
Vladimir Ivannikov <vovannikov@yandex.ru>
2023-02-16 15:46:56 +0100

    Add debug initial values

---
+++ applications/sintering/include/pf-applications/sintering/initial_values_debug.h
@@ -0,0 +1,114 @@
+#pragma once
+
+#include "initial_values.h"
+
+namespace Sintering
+{
+  using namespace dealii;
+
+  template <int dim>
+  class InitialValuesDebug : public InitialValues<dim>
+  {
+  public:
+    double
+    do_value(const Point<dim> &p, const unsigned int component) const final
+    {
+      std::vector<double> vals;
+
+      const double tol = 1e-9;
+
+      if constexpr (dim == 2)
+        {
+          Point<dim> p1(0, 0);
+          Point<dim> p2(1, 0);
+          Point<dim> p3(2, 0);
+          Point<dim> p4(0, 1);
+          Point<dim> p5(1, 1);
+          Point<dim> p6(2, 1);
+
+          if (p.distance(p1) < tol)
+            vals = {1.0, 0.1, 1.0, 0.0};
+          else if (p.distance(p2) < tol)
+            vals = {0.8, 0.2, 0.4, 0.4};
+          else if (p.distance(p3) < tol)
+            vals = {1.0, 0.3, 0.0, 1.0};
+          else if (p.distance(p4) < tol)
+            vals = {1.0, 0.4, 1.0, 0.0};
+          else if (p.distance(p5) < tol)
+            vals = {0.8, 0.5, 0.4, 0.4};
+          else if (p.distance(p6) < tol)
+            vals = {1.0, 0.6, 0.0, 1.0};
+          else
+            {
+              std::cout << "Point = " << p << std::endl;
+              throw std::runtime_error("Wrong point!");
+            }
+        }
+      else
+        {
+          Point<dim> p1(0, 0, 0);
+          Point<dim> p2(1, 0, 0);
+          Point<dim> p3(2, 0, 0);
+          Point<dim> p4(0, 1, 0);
+          Point<dim> p5(1, 1, 0);
+          Point<dim> p6(2, 1, 0);
+          Point<dim> p7(0, 0, 1);
+          Point<dim> p8(1, 0, 1);
+          Point<dim> p9(2, 0, 1);
+          Point<dim> p10(0, 1, 1);
+          Point<dim> p11(1, 1, 1);
+          Point<dim> p12(2, 1, 1);
+
+          if (p.distance(p1) < tol || p.distance(p7) < tol)
+            vals = {1.0, 0.1, 1.0, 0.0};
+          else if (p.distance(p2) < tol || p.distance(p8) < tol)
+            vals = {0.8, 0.2, 0.4, 0.4};
+          else if (p.distance(p3) < tol || p.distance(p9) < tol)
+            vals = {1.0, 0.3, 0.0, 1.0};
+          else if (p.distance(p4) < tol || p.distance(p10) < tol)
+            vals = {1.0, 0.4, 1.0, 0.0};
+          else if (p.distance(p5) < tol || p.distance(p11) < tol)
+            vals = {0.8, 0.5, 0.4, 0.4};
+          else if (p.distance(p6) < tol || p.distance(p12) < tol)
+            vals = {1.0, 0.6, 0.0, 1.0};
+          else
+            {
+              std::cout << "Point = " << p << std::endl;
+              throw std::runtime_error("Wrong point!");
+            }
+        }
+
+      return component < 4 ? vals[component] : 0.0;
+    }
+
+    std::pair<Point<dim>, Point<dim>>
+    get_domain_boundaries() const final
+    {
+      return {};
+    }
+
+    double
+    get_r_max() const final
+    {
+      return 1.;
+    }
+
+    double
+    get_interface_width() const final
+    {
+      return 1.;
+    }
+
+    unsigned int
+    n_order_parameters() const final
+    {
+      return 2;
+    }
+
+    unsigned int
+    n_particles() const final
+    {
+      return 2;
+    }
+  };
+} // namespace Sintering
