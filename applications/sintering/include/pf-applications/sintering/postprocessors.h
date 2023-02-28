c96005a1ebe157e418a4b40ec45d00ed399d70a6
Vladimir Ivannikov <vovannikov@yandex.ru>
2023-02-28 11:14:00 +0100

    Add filter to surface contours vtu

--- applications/sintering/include/pf-applications/sintering/postprocessors.h
+++ applications/sintering/include/pf-applications/sintering/postprocessors.h
@@ -485,6 +485,7 @@ namespace Sintering
       const unsigned int                        n_grains,
       const GrainTracker::Tracker<dim, Number> &grain_tracker_in,
       const unsigned int                        n_coarsening_steps = 0,
+      std::function<int(const Point<dim> &)>    box_filter         = nullptr,
       const unsigned int                        n_subdivisions     = 1,
       const double                              tolerance          = 1e-10)
     {
@@ -529,6 +530,53 @@ namespace Sintering
             grain_tracker->track(vector, n_grains, true);
         }
 
+      if (box_filter)
+        {
+          // Copy vector if not doen before
+          if (n_coarsening_steps == 0)
+            {
+              solution_dealii = vector;
+              solution_dealii.update_ghost_values();
+              vector_to_be_used = &solution_dealii;
+            }
+
+          const auto &  fe = background_dof_handler_to_be_used->get_fe();
+          FEValues<dim> fe_values(mapping,
+                                  fe,
+                                  fe.get_unit_support_points(),
+                                  update_quadrature_points);
+
+          std::vector<types::global_dof_index> dof_indices(
+            fe.n_dofs_per_cell());
+
+          for (const auto &cell :
+               background_dof_handler_to_be_used->active_cell_iterators())
+            {
+              if (!cell->is_locally_owned())
+                continue;
+
+              cell->get_dof_indices(dof_indices);
+
+              fe_values.reinit(cell);
+
+              for (unsigned int b = 0; b < n_grains; ++b)
+                {
+                  const auto &points = fe_values.get_quadrature_points();
+
+                  for (unsigned int i = 0; i < fe.n_dofs_per_cell(); ++i)
+                    {
+                      const int pred_status = box_filter(points[i]);
+
+                      auto &global_dof_value =
+                        solution_dealii.block(b + 2)[dof_indices[i]];
+                      if (pred_status == 0)
+                        global_dof_value = std::min(global_dof_value, 0.5);
+                      else if (pred_status == -1)
+                        global_dof_value = 0.;
+                    }
+                }
+            }
+        }
 
 
       // step 1) create surface mesh
