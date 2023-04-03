fc3b9dc1b3891acddf79320c48077e4c26b08008
Vladimir Ivannikov <vovannikov@yandex.ru>
2023-04-03 17:49:36 +0200

    Add test for generic wo rbm

---
+++ tests/tangent_generic_no_rbm.cc
@@ -0,0 +1,33 @@
+#define MAX_SINTERING_GRAINS 2
+#define FE_DEGREE 1
+#define N_Q_POINTS_1D FE_DEGREE + 1
+
+#include <deal.II/base/mpi.h>
+
+#include <pf-applications/sintering/operator_sintering_generic.h>
+
+#include "include/tangent_tester.h"
+
+using namespace dealii;
+using namespace Sintering;
+
+using Number              = double;
+using VectorizedArrayType = VectorizedArray<Number>;
+
+int
+main(int argc, char **argv)
+{
+  Utilities::MPI::MPI_InitFinalize mpi_init(argc, argv, 1);
+
+  constexpr int dim = 2;
+
+  using NonLinearOperator =
+    SinteringOperatorGeneric<dim, Number, VectorizedArrayType>;
+
+  const bool   enable_rbm = false;
+  const double tol_abs    = 1e-3;
+  const double tol_rel    = 1e-6;
+
+  Test::check_tangent<dim, Number, VectorizedArrayType, NonLinearOperator>(
+    enable_rbm, tol_abs, tol_rel);
+}
