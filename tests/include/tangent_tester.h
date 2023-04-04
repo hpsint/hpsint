a8de107ed135964f3e659f33e88775bd68d65c5b
Vladimir Ivannikov <vovannikov@yandex.ru>
2023-04-04 03:01:41 +0200

    Extract function for numeric tangent

--- tests/include/tangent_tester.h
+++ tests/include/tangent_tester.h
@@ -26,6 +26,70 @@ namespace Test
   using namespace Sintering;
 
   template <int dim,
+            typename VectorType,
+            typename MatrixType,
+            typename NonLinearOperator>
+  void
+  calc_numeric_tangent(
+    const DoFHandler<dim> &                               dof_handler,
+    const NonLinearOperator &                             nonlinear_operator,
+    const VectorType &                                    linearization_point,
+    std::function<void(const VectorType &, VectorType &)> nl_residual,
+    MatrixType &                                          tangent_numeric,
+    const double                                          epsilon   = 1e-7,
+    const double                                          tolerance = 1e-12)
+  {
+    VectorType residual;
+
+    nonlinear_operator.initialize_dof_vector(residual);
+    nl_residual(linearization_point, residual);
+
+    const VectorType residual0(residual);
+    VectorType       state(linearization_point);
+
+    const auto locally_owned_dofs = dof_handler.locally_owned_dofs();
+    const auto n_blocks           = state.n_blocks();
+
+    for (unsigned int b = 0; b < n_blocks; ++b)
+      for (unsigned int i = 0; i < state.block(b).size(); ++i)
+        {
+          VectorType residual1(residual);
+          residual1 = 0;
+
+          if (locally_owned_dofs.is_element(i))
+            state.block(b)[i] += epsilon;
+
+          nl_residual(state, residual1);
+
+          if (locally_owned_dofs.is_element(i))
+            state.block(b)[i] -= epsilon;
+
+          for (unsigned int b_ = 0; b_ < n_blocks; ++b_)
+            for (unsigned int i_ = 0; i_ < state.block(b).size(); ++i_)
+              if (locally_owned_dofs.is_element(i_))
+                {
+                  if (nonlinear_operator.get_sparsity_pattern().exists(
+                        b_ + i_ * n_blocks, b + i * n_blocks))
+                    {
+                      const auto value =
+                        (residual1.block(b_)[i_] - residual0.block(b_)[i_]) /
+                        epsilon;
+
+                      if (std::abs(value) > tolerance)
+                        tangent_numeric.set(b_ + i_ * n_blocks,
+                                            b + i * n_blocks,
+                                            value);
+
+                      else if ((b == b_) && (i == i_))
+                        tangent_numeric.set(b_ + i_ * n_blocks,
+                                            b + i * n_blocks,
+                                            1.0);
+                    }
+                }
+        }
+  }
+
+  template <int dim,
             typename Number,
             typename VectorizedArrayType,
             typename NonLinearOperator>
@@ -209,74 +273,38 @@ namespace Test
 
     grain_tracker.initial_setup(solution, sintering_data.n_grains());
 
-    advection_operator.evaluate_forces(solution, advection_mechanism);
-
     const bool save_all_blocks = true;
     sintering_data.fill_quadrature_point_values(matrix_free,
                                                 solution,
                                                 enable_rbm,
                                                 save_all_blocks);
 
-    const double epsilon   = 1e-7;
-    const double tolerance = 1e-12;
+    // How to compute residual
+    const std::function<void(const VectorType &, VectorType &)> nl_residual =
+      [&](const auto &src, auto &dst) {
+        if (enable_rbm)
+          advection_operator.evaluate_forces(src, advection_mechanism);
 
+        nonlinear_operator.evaluate_nonlinear_residual(dst, src);
+      };
+
+    // Evaluate residual (used for debug and to init advection data structures)
     VectorType residual;
     nonlinear_operator.initialize_dof_vector(residual);
-
-    nonlinear_operator.evaluate_nonlinear_residual(residual, solution);
+    nl_residual(solution, residual);
 
     const unsigned int n_blocks = solution.n_blocks();
+    const unsigned int n_dofs   = dof_handler.n_dofs() * n_blocks;
 
-    const unsigned int n_dofs = dof_handler.n_dofs() * n_blocks;
-
+    // At first compute analytic matrix
     FullMatrix<Number> tangent_analytic(n_dofs, n_dofs);
     nonlinear_operator.initialize_system_matrix(false);
     tangent_analytic.copy_from(nonlinear_operator.get_system_matrix());
 
-    const auto         residual0(residual);
+    // Then compute analytic matrix
     FullMatrix<Number> tangent_numeric(n_dofs, n_dofs);
-
-    const auto locally_owned_dofs = dof_handler.locally_owned_dofs();
-
-    for (unsigned int b = 0; b < n_blocks; ++b)
-      for (unsigned int i = 0; i < solution.block(b).size(); ++i)
-        {
-          auto residual1(residual);
-          residual1 = 0;
-
-          if (locally_owned_dofs.is_element(i))
-            solution.block(b)[i] += epsilon;
-
-          advection_operator.evaluate_forces(solution, advection_mechanism);
-
-          nonlinear_operator.evaluate_nonlinear_residual(residual1, solution);
-
-          if (locally_owned_dofs.is_element(i))
-            solution.block(b)[i] -= epsilon;
-
-          for (unsigned int b_ = 0; b_ < n_blocks; ++b_)
-            for (unsigned int i_ = 0; i_ < solution.block(b).size(); ++i_)
-              if (locally_owned_dofs.is_element(i_))
-                {
-                  if (nonlinear_operator.get_sparsity_pattern().exists(
-                        b_ + i_ * n_blocks, b + i * n_blocks))
-                    {
-                      const Number value =
-                        (residual1.block(b_)[i_] - residual0.block(b_)[i_]) /
-                        epsilon;
-
-                      if (std::abs(value) > tolerance)
-                        tangent_numeric.set(b_ + i_ * n_blocks,
-                                            b + i * n_blocks,
-                                            value);
-
-                      else if ((b == b_) && (i == i_))
-                        tangent_numeric.set(b_ + i_ * n_blocks,
-                                            b + i * n_blocks,
-                                            1.0);
-                    }
-                }
-        }
+    calc_numeric_tangent(
+      dof_handler, nonlinear_operator, solution, nl_residual, tangent_numeric);
 
     auto tangent_diff(tangent_analytic);
     tangent_diff.add(Number(-1.), tangent_numeric);
