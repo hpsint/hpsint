d4981c3209f12f90860ad30cd9257abdeb2db549
Vladimir Ivannikov <vovannikov@yandex.ru>
2023-04-11 23:01:42 +0200

    Add tests for scalar

---
+++ tests/residual_generic_scalar_no_rbm.cc
@@ -0,0 +1,28 @@
+#define MAX_SINTERING_GRAINS 2
+#define FE_DEGREE 1
+#define N_Q_POINTS_1D FE_DEGREE + 1
+
+#include <deal.II/base/mpi.h>
+
+#include <pf-applications/sintering/operator_sintering_generic.h>
+
+#include "include/residual_tester.h"
+
+using namespace dealii;
+using namespace Sintering;
+
+using Number              = double;
+using VectorizedArrayType = VectorizedArray<Number>;
+
+int
+main(int argc, char **argv)
+{
+  Utilities::MPI::MPI_InitFinalize mpi_init(argc, argv, 1);
+
+  constexpr int dim = 2;
+
+  using NonLinearOperator =
+    SinteringOperatorGeneric<dim, Number, VectorizedArrayType>;
+
+  Test::calc_residual<dim, Number, VectorizedArrayType, NonLinearOperator>(false);
+}
