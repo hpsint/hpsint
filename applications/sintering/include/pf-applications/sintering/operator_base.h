8d3537118a059d4d0af173f531e7cef906281d71
Vladimir Ivannikov <vovannikov@yandex.ru>
2023-05-15 18:04:11 +0200

    Improve instantiation

--- applications/sintering/include/pf-applications/sintering/operator_base.h
+++ applications/sintering/include/pf-applications/sintering/operator_base.h
@@ -10,89 +10,12 @@
 #include <pf-applications/numerics/functions.h>
 #include <pf-applications/numerics/vector_tools.h>
 
-#include <boost/preprocessor.hpp>
-#include <boost/preprocessor/repetition/repeat.hpp>
-#include <boost/preprocessor/repetition/repeat_from_to.hpp>
+#include <pf-applications/sintering/instantiation.h>
 
 #include <pf-applications/dofs/dof_tools.h>
 #include <pf-applications/matrix_free/tools.h>
 
 #include <fstream>
-
-template <typename T>
-using n_grains_t = decltype(std::declval<T const>().n_grains());
-
-template <typename T>
-using n_grains_to_n_components_t =
-  decltype(std::declval<T const>().n_grains_to_n_components(
-    std::declval<const unsigned int>()));
-
-template <typename T>
-constexpr bool has_n_grains_method =
-  dealii::internal::is_supported_operation<n_grains_t, T>
-    &&dealii::internal::is_supported_operation<n_grains_to_n_components_t, T>;
-
-DeclException3(
-  ExcInvalidNumberOfComponents,
-  unsigned int,
-  unsigned int,
-  unsigned int,
-  << "This operation is implemented for the number of components in range ["
-  << arg1 << ", " << arg2 << "] "
-  << "whereas you provided n_grains = " << arg3);
-
-// clang-format off
-/**
- * Macro that converts a runtime number (n_components() or n_grains())
- * to constant expressions that can be used for templating and calles
- * the provided function with the two parameters: 1) number of
- * components and 2) number of grains (if it makes sence; else -1).
- *
- * The relation between number of components and number of grains
- * is encrypted in the method T::n_grains_to_n_components().
- * 
- * The function can be used the following way:
- * ```
- * #define OPERATION(c, d) std::cout << c << " " << d << std::endl;
- * EXPAND_OPERATIONS(OPERATION);
- * #undef OPERATION
- * ```
- */
-
-#define EXPAND_CONST(z, n, data) \
-  case n: data(T::n_grains_to_n_components(std::min(max_grains, n)), std::min(max_grains, n)); break;
-
-#define EXPAND_NONCONST(z, n, data) \
-  case n: data(std::min(max_components, n), -1); break;
-
-#define EXPAND_MAX_SINTERING_COMPONENTS \
-  BOOST_PP_ADD(MAX_SINTERING_GRAINS, 2)
-
-#define EXPAND_OPERATIONS(OPERATION)                                                                                  \
-  if constexpr(has_n_grains_method<T>)                                                                                \
-    {                                                                                                                 \
-      constexpr int max_grains = MAX_SINTERING_GRAINS;                                                                \
-      const unsigned int n_grains = static_cast<const T&>(*this).n_grains();                                          \
-      AssertIndexRange(n_grains, max_grains + 1);                                                                     \
-      switch (n_grains)                                                                                               \
-        {                                                                                                             \
-          BOOST_PP_REPEAT(BOOST_PP_INC(MAX_SINTERING_GRAINS), EXPAND_CONST, OPERATION);                               \
-          default:                                                                                                    \
-            AssertThrow(false, ExcInvalidNumberOfComponents(0, MAX_SINTERING_GRAINS, n_grains));                      \
-        }                                                                                                             \
-    }                                                                                                                 \
-  else                                                                                                                \
-    {                                                                                                                 \
-      constexpr int max_components = MAX_SINTERING_GRAINS + 2;                                                        \
-      AssertIndexRange(this->n_components(), max_components + 1);                                                     \
-      switch (this->n_components())                                                                                   \
-        {                                                                                                             \
-          BOOST_PP_REPEAT_FROM_TO(1, BOOST_PP_INC(EXPAND_MAX_SINTERING_COMPONENTS), EXPAND_NONCONST, OPERATION);      \
-          default:                                                                                                    \
-            AssertThrow(false, ExcInvalidNumberOfComponents(1, max_components, this->n_components()));                \
-        }                                                                                                             \
-  }
-// clang-format on
 
 namespace Sintering
 {
