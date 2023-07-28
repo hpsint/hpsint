5c09eff4dbb2aaf8ab1ffdf9fbbe4746db629845
Vladimir Ivannikov <vovannikov@yandex.ru>
2023-07-28 08:31:58 +0200

    Improve postprocessing of shrinkage

--- applications/sintering/scripts/postproc_shrinkage.py
+++ applications/sintering/scripts/postproc_shrinkage.py
@@ -2,6 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import argparse
 import colorsys
+import glob
+import re
+import sys
 import collections.abc
 
 def get_hex_colors(N):
@@ -13,16 +16,32 @@ def get_hex_colors(N):
     return hex_out
 
 parser = argparse.ArgumentParser(description='Process shrinkage data')
-parser.add_argument("-f", "--files", dest="files", nargs='+', required=True, help="Source filename")
+parser.add_argument("-f", "--files", dest="files", nargs='+', required=True, help="Source filenames, can be defined as masks")
 parser.add_argument("-d", "--directions", dest="directions", nargs='+',
     required=False, default="all", help="Directions", choices=['x', 'y', 'z', 'vol', 'all'])
+parser.add_argument("-t", "--limit_t", dest='limit_t', required=False, help="Max time limit", type=float, default=sys.float_info.max)
+parser.add_argument("-m", "--markers", dest='markers', required=False, help="Number of markers", type=int, default=30)
+parser.add_argument("-c", "--collapse", dest='collapse', required=False, help="Number of markers", action="store_true", default=False)
 
 args = parser.parse_args()
 
 if not isinstance(args.directions, collections.abc.Sequence):
     args.directions = [args.directions]
 
-n_files = len(args.files)
+# Get all files to process
+files_list = []
+for fm in args.files:
+    current_files_list = glob.glob(fm, recursive=True)
+    current_files_list.sort(key=lambda f: int(re.sub('\D', '', f)))
+    files_list += current_files_list
+
+files_list = sorted(list(set(files_list)))
+
+print("The complete list of files to process:")
+for f in files_list:
+    print(f)
+
+n_files = len(files_list)
 colors = get_hex_colors(n_files)
 
 csv_header = ["dim_x", "dim_y", "dim_z", "volume", "shrinkage_x", "shrinkage_y", "shrinkage_z", "densification"]
@@ -30,14 +49,50 @@ n_qtys = 4
 markers = ["s", "D", "o", "x"]
 
 if 'all' in args.directions:
-    active = True*[n_qtys]
+    active = [True]*n_qtys
 else:
     active = [(item in args.directions) for item in ['x', 'y', 'z', 'vol']]
 
 fig, axes = plt.subplots(nrows=1, ncols=2)
 fig.suptitle('Shrinkage and densification')
 
-for f, clr in zip(args.files, colors):
+# Generate labels
+if args.collapse:
+    min_len = min([len(f) for f in files_list])
+
+    s_start = 0
+    s_end = 0
+    
+    for i in range(min_len):
+        string_equal = True
+        for f in files_list:
+            if not(f[i] == files_list[0][i]):
+                string_equal = False
+                break
+
+        if string_equal:
+            s_start += 1
+        else:
+            break
+
+    for i in range(min_len):
+        string_equal = True
+        for f in files_list:
+            if not(f[-1-i] == files_list[0][-1-i]):
+                string_equal = False
+                break
+
+        if string_equal:
+            s_end += 1
+        else:
+            break
+
+    labels = [f[s_start:-s_end] for f in files_list]
+
+else:
+    labels = files_list
+
+for f, lbl, clr in zip(files_list, labels, colors):
 
     fdata = np.genfromtxt(f, dtype=None, names=True)
 
@@ -50,9 +105,21 @@ for f, clr in zip(args.files, colors):
             ref0 = fdata[qty_name][0]
             ref_qty = (ref0 - fdata[qty_name]) / ref0
 
-            m_type = markers[i % len(markers)]
-            axes[0].plot(fdata["time"], fdata[qty_name], label=" ".join([f, csv_header[i]]), marker=m_type, color=clr, alpha=alpha)
-            axes[1].plot(fdata["time"], ref_qty, label=" ".join([f, csv_header[i+n_qtys]]), marker=m_type, color=clr, alpha=alpha)
+            mask = fdata["time"] < args.limit_t
+
+            if args.markers > 0:
+                n_every = round(len(fdata["time"][mask]) / args.markers)
+                n_every = max(1, n_every)
+
+                m_type = markers[i % len(markers)]
+            else:
+                n_every = 1
+                m_type = None
+
+            axes[0].plot(fdata["time"][mask], fdata[qty_name][mask], label=" ".join([lbl, csv_header[i]]), 
+                marker=m_type, color=clr, alpha=alpha, markevery=n_every)
+            axes[1].plot(fdata["time"][mask], ref_qty[mask], label=" ".join([lbl, csv_header[i+n_qtys]]),
+                marker=m_type, color=clr, alpha=alpha, markevery=n_every)
 
             alpha -= 0.2
 
