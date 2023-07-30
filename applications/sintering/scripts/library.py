69888dbac0b4ff7022b1bfb64d8b9dffca259de8
Vladimir Ivannikov <vovannikov@yandex.ru>
2023-07-30 13:42:59 +0200

    Extract function to library

---
+++ applications/sintering/scripts/library.py
@@ -0,0 +1,9 @@
+import colorsys
+
+def get_hex_colors(N):
+    hsv_tuples = [(x * 1.0 / N, 0.5, 0.5) for x in range(N)]
+    hex_out = []
+    for rgb in hsv_tuples:
+        rgb = map(lambda x: int(x * 255), colorsys.hsv_to_rgb(*rgb))
+        hex_out.append('#%02x%02x%02x' % tuple(rgb))
+    return hex_out
--- applications/sintering/scripts/postproc_shrinkage.py
+++ applications/sintering/scripts/postproc_shrinkage.py
@@ -1,19 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
-import colorsys
 import glob
 import re
 import sys
 import collections.abc
-
-def get_hex_colors(N):
-    hsv_tuples = [(x * 1.0 / N, 0.5, 0.5) for x in range(N)]
-    hex_out = []
-    for rgb in hsv_tuples:
-        rgb = map(lambda x: int(x * 255), colorsys.hsv_to_rgb(*rgb))
-        hex_out.append('#%02x%02x%02x' % tuple(rgb))
-    return hex_out
+from library import get_hex_colors
 
 parser = argparse.ArgumentParser(description='Process shrinkage data')
 parser.add_argument("-f", "--files", dest="files", nargs='+', required=True, help="Source filenames, can be defined as masks")
