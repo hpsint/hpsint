9e3cd5d01e2c5db8e2aabc5d40a95c89b98ff881
Vladimir Ivannikov <vovannikov@yandex.ru>
2023-08-27 23:13:12 +0200

    Add tests

---
+++ tests/initial_mesh_102p_from_max_radius.cc
@@ -0,0 +1,31 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2023 by the hpsint authors
+//
+// This file is part of the hpsint library.
+//
+// The hpsint library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 3.0 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE.MD at
+// the top level directory of hpsint.
+//
+// ---------------------------------------------------------------------
+
+#include <deal.II/base/mpi.h>
+
+#include <pf-applications/sintering/tools.h>
+
+#include "include/mesh_tester.h"
+
+using namespace dealii;
+using namespace Sintering;
+
+int
+main(int argc, char **argv)
+{
+  Utilities::MPI::MPI_InitFinalize mpi_init(argc, argv, 1);
+
+  Test::mesh_cloud("102particles.cloud", InitialMesh::MaxRadius);
+}
