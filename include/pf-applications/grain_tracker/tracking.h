79faa3daae3c8ed9027febdd4ee105c3f43ead9e
Vladimir Ivannikov <vovannikov@yandex.ru>
2023-09-05 15:41:23 +0200

    Manual sorting

--- include/pf-applications/grain_tracker/tracking.h
+++ include/pf-applications/grain_tracker/tracking.h
@@ -92,16 +92,55 @@ namespace GrainTracker
 
               std::vector<typename ContainerType::size_type> result;
 
-              tree.query(bgi::intersects(box) &&
-                           bgi::nearest(old_segment.get_center(), 1),
-                         std::back_inserter(result));
+              tree.query(bgi::intersects(box), std::back_inserter(result));
 
               // If any of the grain was identified, then there is no need to
               // check for the others, so we break the iteration
               if (!result.empty())
                 {
-                  new_grains_to_old.at(new_segments_to_grains[result[0]]) =
-                    grain_id;
+                  /* Perform manual sorting among the candidates, since
+                   * bgi::nearest() outputs the closest to the face of the box,
+                   * not the box having the closest center. */
+
+                  unsigned int new_grain_id = 0;
+
+                  if (result.size() > 0)
+                    {
+                      double min_dist = std::numeric_limits<double>::max();
+
+                      for (const auto &id : result)
+                        {
+                          const unsigned int candidate_grain_id =
+                            new_segments_to_grains[id];
+
+                          // Compute the segment id
+                          unsigned int work_id = id;
+                          for (; work_id > 0 &&
+                                 new_segments_to_grains[work_id - 1] ==
+                                   new_segments_to_grains[work_id];
+                               --work_id)
+                            ;
+                          const unsigned int segment_id = id - work_id;
+
+                          const auto &candidate_segment =
+                            new_grains.at(candidate_grain_id)
+                              .get_segments()[segment_id];
+
+                          const auto curent_dist =
+                            candidate_segment.get_center().distance(
+                              old_segment.get_center());
+
+                          if (curent_dist < min_dist)
+                            {
+                              new_grain_id = candidate_grain_id;
+                              min_dist     = curent_dist;
+                            }
+                        }
+                    }
+                  else
+                    new_grain_id = result[0];
+
+                  new_grains_to_old.at(new_grain_id) = grain_id;
                   break;
                 }
             }
