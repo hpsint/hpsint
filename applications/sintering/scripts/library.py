d9fc93deadf2d11a129a0e0a30f7efd33525155d
Vladimir Ivannikov <vovannikov@yandex.ru>
2024-01-22 21:18:02 +0100

    Add clean_folder to library

--- applications/sintering/scripts/library.py
+++ applications/sintering/scripts/library.py
@@ -153,3 +153,14 @@ def animation_format_plot(ax, main_color, background_color, ticks_size, axes_lab
     ax.set_facecolor(background_color)
     ax.xaxis.label.set_size(axes_label_size)
     ax.yaxis.label.set_size(axes_label_size)
+
+def clean_folder(folder):
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
