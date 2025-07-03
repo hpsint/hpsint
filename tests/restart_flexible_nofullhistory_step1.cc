1afb7662fca8b8bd1ce11a15201119a2d293c22f
Vladimir Ivannikov <vladimir.ivannikov@hereon.de>
2025-07-03 10:11:22 +0200

    Add restart tests

---
+++ tests/restart_flexible_nofullhistory_step1.cc
@@ -0,0 +1,45 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2025 by the hpsint authors
+//
+// This file is part of the hpsint library.
+//
+// The hpsint library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 3.0 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE.MD at
+// the top level directory of hpsint.
+//
+// ---------------------------------------------------------------------
+
+#include <deal.II/base/mpi.h>
+
+#include <pf-applications/tests/runner_sintering_models.h>
+
+#include <string>
+#include <vector>
+
+using namespace dealii;
+using namespace Test;
+
+int
+main(int argc, char **argv)
+{
+  Utilities::MPI::MPI_InitFinalize mpi_init(argc, argv, 1);
+
+  std::vector<std::string> arguments = {
+    "dummy",
+    "--restart",
+    std::string("../../restart_flexible_nofullhistory_step0.") +
+      XSTRING(OUTPUT_FOLDER_SUFFIX) + "/mpirun=2/restart_0",
+    "--PrintTimeLoop=false",
+    "--TimeIntegration.TimeEnd=2e-1",
+    "--TimeIntegration.TimeStepInit=1e-2",
+    "--Restart.FlexibleOutput=true",
+    "--Restart.FullHistory=false"};
+
+  run_sintering_operator_generic(arguments.begin(), arguments.end());
+
+  return 0;
+}
