2cd6e1038fdd1ce33bbd0d794861b2fd64787520
Vladimir Ivannikov <vladimir.ivannikov@hereon.de>
2025-07-14 11:24:01 +0200

    Add integration scheme variant

--- include/pf-applications/time_integration/time_schemes.h
+++ include/pf-applications/time_integration/time_schemes.h
@@ -51,6 +51,107 @@ namespace TimeIntegration
     }
   };
 
+  template <typename Number, typename Scheme>
+  struct ImplicitSchemeBase : public ImplicitScheme<Number>
+  {
+    virtual std::unique_ptr<ImplicitScheme<Number>>
+    clone() const override
+    {
+      return std::make_unique<Scheme>(static_cast<const Scheme &>(*this));
+    }
+  };
+
+  template <typename Number>
+  struct BDF1Scheme : public ImplicitSchemeBase<Number, BDF1Scheme<Number>>
+  {
+    unsigned int
+    get_order() const override
+    {
+      return 1;
+    }
+
+    virtual std::vector<Number>
+    compute_weights(const std::vector<Number> &dt) const override
+    {
+      std::vector<Number> weights(dt.size() + 1, 0.);
+      if (this->effective_order(dt) == 1)
+        {
+          weights[0] = 1.0 / dt[0];
+          weights[1] = -1.0 / dt[0];
+        }
+
+      return weights;
+    }
+  };
+
+  template <typename Number>
+  struct BDF2Scheme : public ImplicitSchemeBase<Number, BDF2Scheme<Number>>
+  {
+    unsigned int
+    get_order() const override
+    {
+      return 2;
+    }
+
+    virtual std::vector<Number>
+    compute_weights(const std::vector<Number> &dt) const override
+    {
+      std::vector<Number> weights(dt.size() + 1, 0.);
+      if (this->effective_order(dt) == 2)
+        {
+          weights[0] = (2 * dt[0] + dt[1]) / (dt[0] * (dt[0] + dt[1]));
+          weights[1] = -(dt[0] + dt[1]) / (dt[0] * dt[1]);
+          weights[2] = dt[0] / (dt[1] * (dt[0] + dt[1]));
+        }
+      else if (this->effective_order(dt) == 1)
+        {
+          weights[0] = 1.0 / dt[0];
+          weights[1] = -1.0 / dt[0];
+        }
+
+      return weights;
+    }
+  };
+
+  template <typename Number>
+  struct BDF3Scheme : public ImplicitSchemeBase<Number, BDF3Scheme<Number>>
+  {
+    unsigned int
+    get_order() const override
+    {
+      return 3;
+    }
+
+    virtual std::vector<Number>
+    compute_weights(const std::vector<Number> &dt) const override
+    {
+      std::vector<Number> weights(dt.size() + 1, 0.);
+      if (this->effective_order(dt) == 3)
+        {
+          weights[1] = -(dt[0] + dt[1]) * (dt[0] + dt[1] + dt[2]) /
+                       (dt[0] * dt[1] * (dt[1] + dt[2]));
+          weights[2] =
+            dt[0] * (dt[0] + dt[1] + dt[2]) / (dt[1] * dt[2] * (dt[0] + dt[1]));
+          weights[3] = -dt[0] * (dt[0] + dt[1]) /
+                       (dt[2] * (dt[1] + dt[2]) * (dt[0] + dt[1] + dt[2]));
+          weights[0] = -(weights[1] + weights[2] + weights[3]);
+        }
+      else if (this->effective_order(dt) == 2)
+        {
+          weights[0] = (2 * dt[0] + dt[1]) / (dt[0] * (dt[0] + dt[1]));
+          weights[1] = -(dt[0] + dt[1]) / (dt[0] * dt[1]);
+          weights[2] = dt[0] / (dt[1] * (dt[0] + dt[1]));
+        }
+      else if (this->effective_order(dt) == 1)
+        {
+          weights[0] = 1.0 / dt[0];
+          weights[1] = -1.0 / dt[0];
+        }
+
+      return weights;
+    }
+  };
+
   struct ExplicitScheme
   {
     virtual ~ExplicitScheme() = default;
@@ -63,4 +164,42 @@ namespace TimeIntegration
     virtual std::vector<std::pair<double, double>>
     get_stages() const = 0;
   };
+
+  template <typename Number>
+  struct IntegrationSchemeVariant
+  {
+    template <typename T>
+    IntegrationSchemeVariant(std::unique_ptr<T> scheme)
+      : scheme(std::move(scheme))
+    {}
+
+    template <typename T>
+    std::unique_ptr<T>
+    try_take()
+    {
+      if (std::holds_alternative<std::unique_ptr<T>>(scheme))
+        return std::move(std::get<std::unique_ptr<T>>(scheme));
+      else
+        return nullptr;
+    }
+
+  private:
+    std::variant<std::unique_ptr<ImplicitScheme<Number>>,
+                 std::unique_ptr<ExplicitScheme>>
+      scheme;
+  };
+
+  template <typename Number>
+  IntegrationSchemeVariant<Number>
+  create_time_scheme(std::string name)
+  {
+    static std::map<std::string,
+                    std::function<IntegrationSchemeVariant<Number>()>>
+      factory = {
+        {"BDF1", []() { return std::make_unique<BDF1Scheme<Number>>(); }},
+        {"BDF2", []() { return std::make_unique<BDF2Scheme<Number>>(); }},
+        {"BDF3", []() { return std::make_unique<BDF3Scheme<Number>>(); }}};
+
+    return factory.at(name)();
+  }
 } // namespace TimeIntegration
