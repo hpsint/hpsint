4a599497496dfb06807950c901bd23a26a7442bb
Vladimir Ivannikov <vladimir.ivannikov@hereon.de>
2026-02-11 14:01:49 +0100

    Add Arkode instantiations

--- inst.cc
+++ inst.cc
@@ -1,6 +1,15 @@
 #include <deal.II/lac/affine_constraints.h>
 #include <deal.II/lac/affine_constraints.templates.h>
 
+#ifdef DEAL_II_WITH_SUNDIALS
+#  include <deal.II/sundials/arkode.h>
+#  include <deal.II/sundials/arkode.templates.h>
+#  include <deal.II/sundials/n_vector.h>
+#  include <deal.II/sundials/n_vector.templates.h>
+#  include <deal.II/sundials/sunlinsol_wrapper.h>
+#  include <deal.II/sundials/sunlinsol_wrapper.templates.h>
+#endif
+
 #include <deal.II/trilinos/nox.h>
 #include <deal.II/trilinos/nox.templates.h>
 
@@ -17,6 +26,33 @@ template void
 internal::AffineConstraintsImplementation::set_zero_all(
   const std::vector<types::global_dof_index> &,
   LinearAlgebra::distributed::DynamicBlockVector<double> &);
+
+// SUNDIALS wrapper instantiations
+#ifdef DEAL_II_WITH_SUNDIALS
+template LinearAlgebra::distributed::DynamicBlockVector<double> *
+  SUNDIALS::internal::unwrap_nvector<
+    LinearAlgebra::distributed::DynamicBlockVector<double>>(N_Vector);
+template const LinearAlgebra::distributed::DynamicBlockVector<double> *
+  SUNDIALS::internal::unwrap_nvector_const<
+    LinearAlgebra::distributed::DynamicBlockVector<double>>(N_Vector);
+
+template class SUNDIALS::internal::NVectorView<
+  LinearAlgebra::distributed::DynamicBlockVector<double>>;
+template class SUNDIALS::internal::NVectorView<
+  const LinearAlgebra::distributed::DynamicBlockVector<double>>;
+
+template struct SUNDIALS::SundialsOperator<
+  LinearAlgebra::distributed::DynamicBlockVector<double>>;
+
+template struct SUNDIALS::SundialsPreconditioner<
+  LinearAlgebra::distributed::DynamicBlockVector<double>>;
+
+template class SUNDIALS::internal::LinearSolverWrapper<
+  LinearAlgebra::distributed::DynamicBlockVector<double>>;
+
+template class SUNDIALS::ARKode<
+  LinearAlgebra::distributed::DynamicBlockVector<double>>;
+#endif
 
 // Explicitly export instantiations to make polymorphic serialization work
 BOOST_CLASS_EXPORT(GrainTracker::RepresentationSpherical<2>)
