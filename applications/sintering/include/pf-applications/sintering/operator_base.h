3af17b1d81592267650d87dd4164db3ecb778920
Vladimir Ivannikov <vladimir.ivannikov@hereon.de>
2026-05-25 11:00:31 +0200

    Sum only over active lanes

--- applications/sintering/include/pf-applications/sintering/operator_base.h
+++ applications/sintering/include/pf-applications/sintering/operator_base.h
@@ -907,11 +907,14 @@ namespace Sintering
                 }
             }
 
+          const unsigned int n_active_lanes =
+            this->matrix_free.n_active_entries_per_cell_batch(cell);
+
           for (unsigned int i = 0; i < quantities.size(); ++i)
             {
               const auto vals = fe_eval[i].integrate_value();
-              q_values[i] +=
-                std::accumulate(vals.begin(), vals.end(), Number(0));
+              for (unsigned int lane = 0; lane < n_active_lanes; ++lane)
+                q_values[i] += vals[lane];
             }
         }
 
